b435ce8bbd963e8b04f59fc98404305180857793
Gopi Krishna <34994746+bgopikrishna@users.noreply.github.com>
2020-03-21 15:30:55 +0530

    chore: updated server js static file

--- server.js
+++ server.js
@@ -22,8 +22,10 @@ app.use(express.json());
 startMongoose();
 
 if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build/')));
+
     app.get('/', (res, req) => {
-        res.sendFile(path.join(`${__dirname}./client/build/index.html`));
+        res.sendFile(path.join(`${__dirname}/client/build/`));
     });
 }
 
