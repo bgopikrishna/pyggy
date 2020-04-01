8e941a2dbcfe50f4fab41c0316cd2436008939a2
Gopi Krishna <34994746+bgopikrishna@users.noreply.github.com>
2020-04-01 09:50:29 +0530

    setup: backend setup in heroki

--- server/server.js
+++ server/server.js
@@ -31,12 +31,4 @@ app.use('/api/auth', authRouter);
 app.use('/api/goals', auth, goalRouter);
 app.use('/api/user', auth, userRouter);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build/'));
-
-    app.get('/*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
-
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
