bddb3be82fc648c7f19e9cd58d8d4c3596040cec
Gopi Krishna <gopiwayz@gmail.com>
2020-05-02 10:06:46 +0530

    chore: completed the feature

--- client/src/components/pages/GoalDetails/GoalDetails.js
+++ client/src/components/pages/GoalDetails/GoalDetails.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useGoals } from '../../../context/GoalsContext';
 import { useParams } from 'react-router-dom';
+import { formatRelative } from 'date-fns';
+import { RECORD_TYPES } from '../../../constants';
+import FullScreenLoader from '../../common/FullScreenLoader/FullScreenLoader';
 
 function GoalDetails() {
     const { goals } = useGoals();
@@ -8,7 +11,13 @@ function GoalDetails() {
 
     const goal = goals.find((g) => g._id === id);
 
-    const { name, favourite, description } = goal;
+    if (!goal) {
+        return <FullScreenLoader />;
+    }
+
+    const { name, favourite, description, records = [] } = goal;
+
+    records.reverse();
 
     return (
         <div>
@@ -17,6 +26,34 @@ function GoalDetails() {
             <p>
                 <strong>Favourite: </strong> {favourite ? 'YES' : 'NO'}
             </p>
+
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {records.map((rec) => (
+                        <tr
+                            className={
+                                RECORD_TYPES.DEPOSIT === rec.recordType
+                                    ? 'has-text-success'
+                                    : 'has-text-danger'
+                            }>
+                            <td>
+                                {formatRelative(new Date(rec.time), Date.now())}
+                            </td>
+                            <td>
+                                {rec.recordType === RECORD_TYPES.DEPOSIT
+                                    ? rec.amount
+                                    : 0 - parseInt(rec.amount)}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 }
